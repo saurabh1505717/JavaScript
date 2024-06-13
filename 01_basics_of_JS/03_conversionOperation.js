10eb175ed02d86ecca254f2dbf888eb61e17746e
Saurabh Kumar <84388992+saurabh1505717@users.noreply.github.com>
2024-06-13 02:47:19 +0000

    conversion of datatypes

---
+++ 01_basics_of_JS/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = 33 
+// let score = "33"
+// let score = "33abc"
+// let score=null;
+// let score=undefined
+// let score=true
+let score="Saurabh"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(typeof(valueInNumber));
+
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1 // true
+// let isLoggedIn = "Saurabh" // true
+// let isLoggedIn="" //false
+let isLoggedIn; // false;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumner=33
+let stringNumber = String(someNumner)
+console.log(stringNumber);
+console.log(typeof stringNumber);
