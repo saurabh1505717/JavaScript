ccaa786ff338130a5546f33c6dffc58e8a7f41d2
Saurabh Kumar <84388992+saurabh1505717@users.noreply.github.com>
2024-06-13 02:30:24 +0000

    datatypes

---
+++ 01_basics_of_JS/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14453
+let accountEmail = "saurabh@google.com"
+var accountPwd = "122345"
+accuntCity = "Jamnagar"
+let accountState;
+
+// accountId = 322412 // not allowed
+
+accountEmail = "sk@sk.com"
+accountPwd="159786"
+accountCity="Kharar"
+
+// console.log(accountId)
+// console.log(accountEmail)
+// console.log(accountPwd)
+// console.log(accountCity)
+
+console.table([accountId, accountEmail, accountPwd, accountCity, accountState])
---
+++ 01_basics_of_JS/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log("Saurabh");
+
+let name = "saurabh"
+let age=18
+let isLoggedIn=false
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof "Saurabh");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
